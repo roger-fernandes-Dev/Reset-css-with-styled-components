c788753b6515a6a6d0e81fb608e5082e411a94d0
Roger Fernandes <99224236+roger-fernandes-Dev@users.noreply.github.com>
2022-03-31 20:19:24 -0300

    Add files via upload

---
+++ _app.js
@@ -0,0 +1,21 @@
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+const GlobalStyled = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: red;
+  }
+
+`
+function MyApp({ Component, pageProps }) {
+  return (
+      <>
+        <GlobalStyled />
+        <Component {...pageProps} />
+      </>
+
+  )
+}
+
+export default MyApp
